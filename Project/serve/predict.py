173155b586202fbb711f0611fec224c6c62472d8
EC2 Default User <ec2-user@ip-172-16-67-188.us-east-2.compute.internal>
2020-07-22 23:55:18 +0000

    updated udacity file

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -69,9 +69,8 @@ def predict_fn(input_data, model):
     #       You should produce two variables:
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
-
-    data_X = None
-    data_len = None
+    test_data = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, test_data)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +86,6 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
+    result = np.round(model.forward(data).detach().numpy()).astype(int)
 
     return result
