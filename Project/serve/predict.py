c63d2a9e3d09a9cb64439a09863bb3f2757cad31
Denys Lazarenko <denis.lazarenko@gmail.com>
2020-12-29 22:06:54 +0200

    Inference: implement predict_fn

--- Project/serve/predict.py
+++ Project/serve/predict.py
@@ -70,8 +70,8 @@ def predict_fn(input_data, model):
     #         data_X   - A sequence of length 500 which represents the converted review
     #         data_len - The length of the review
 
-    data_X = None
-    data_len = None
+    words = review_to_words(input_data)
+    data_X, data_len = convert_and_pad(model.word_dict, words)
 
     # Using data_X and data_len we construct an appropriate input tensor. Remember
     # that our model expects input data of the form 'len, review[500]'.
@@ -87,6 +87,7 @@ def predict_fn(input_data, model):
     # TODO: Compute the result of applying the model to the input data. The variable `result` should
     #       be a numpy array which contains a single integer which is either 1 or 0
 
-    result = None
-
+    res = model(data).detach()
+    print(res)
+    result = res.round().numpy().astype(int)
     return result
